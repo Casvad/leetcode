d7d64776db0229aeea10c86b0208d8ddf26ce995
carlos <carlos@sumerlabs.com>
2023-04-06 22:53:28 -0500

    optimal partition of string

--- minimize-maximum-of-array/main.go
+++ minimize-maximum-of-array/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -11,8 +12,9 @@ func main() {
 	//runExample([]int{13, 13, 20, 0, 8, 9, 9})                                                                                                                                                                            //16
 	//runExample([]int{6, 9, 3, 8, 14})                                                                                                                                                                                    //8
 	//runExample([]int{4, 7, 2, 2, 9, 19, 16, 0, 3, 15})                                                                                                                                                                   //9
-	runExample([]int{439, 228, 482, 150, 231, 209, 991, 125, 453, 407, 670, 491, 300, 125, 285, 749, 350, 411, 527, 768}) //439                                                                            //9
+	//runExample([]int{439, 228, 482, 150, 231, 209, 991, 125, 453, 407, 670, 491, 300, 125, 285, 749, 350, 411, 527, 768}) //439                                                                            //9
 	//runExample([]int{621, 277, 638, 960, 972, 544, 994, 865, 352, 801, 171, 973, 959, 569, 182, 367, 471, 789, 929, 707, 735, 511, 781, 146, 855, 947, 82, 29, 671, 28, 570, 154, 651, 168, 28, 27, 404, 811, 649, 228}) //734
+	runExample([]int{13, 13, 20, 0, 8, 9, 9})
 }
 
 func runExample(input []int) {
@@ -21,6 +23,22 @@ func runExample(input []int) {
 }
 
 func minimizeArrayValue(nums []int) int {
+
+	max := nums[0]
+	totalSum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		totalSum += nums[i]
+		possibleMax := int(math.Ceil(float64(totalSum) / float64(i+1)))
+		if possibleMax > max {
+			max = possibleMax
+		}
+	}
+
+	return max
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+func minimizeArrayValueV2(nums []int) int {
 	maxElementIndex := findMaxElementIndex(nums)
 	maxValue := nums[maxElementIndex]
 	minElementIndex := findMinimumIndexBefore(nums, maxElementIndex)
@@ -73,6 +91,7 @@ func findMinimumIndexBefore(nums []int, limit int) int {
 	return minElementIndex
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////
 ///This solution has speed problems because of the massive iterations to reach for the solution
 func minimizeArrayValueV1(nums []int) int {
 
