c411d65297b7981decd647034b60ac8a7f103b8e
carlos <carlos@sumerlabs.com>
2023-05-08 12:34:32 -0500

    regular expression matching

--- palindrome-number/main.go
+++ palindrome-number/main.go
@@ -6,44 +6,94 @@ import (
 
 func main() {
 
-	runExample(121)
-	runExample(-121)
-	runExample(10)
+	runExample("a", ".*..a*")                                  //true
+	runExample("aaaaaaaaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*") //true
+	runExample("a", "ab*")                                     //true
+	runExample("aaa", "a.a")                                   //true
+	runExample("ab", ".*c")                                    //false
+	runExample("mississippi", "mis*is*p*.")                    //false
+	runExample("aa", "a")                                      //false
+	runExample("aa", "a*")                                     //true
+	runExample("ab", ".*")                                     //true
+	runExample("aab", "c*a*b")                                 //true
 }
 
-func runExample(numRows int) {
-	result := isPalindrome(numRows)
-	fmt.Printf("The result of %v is %v \n", numRows, result)
+func runExample(s string, p string) {
+	result := isMatch(s, p)
+	fmt.Printf("The result of %v %v is %v \n", s, p, result)
 }
 
-func isPalindrome(x int) bool {
+func isMatch(s string, p string) bool {
+	m := len(s)
+	n := len(p)
 
-	digits := map[int]string{0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
+	dp := make([][]bool, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]bool, n+1)
+	}
+	dp[0][0] = true
 
-	if x < 0 {
+	for j := 1; j <= n; j++ {
+		if p[j-1] == '*' {
+			dp[0][j] = dp[0][j-2]
+		}
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if s[i-1] == p[j-1] || p[j-1] == '.' {
+				dp[i][j] = dp[i-1][j-1]
+			} else if p[j-1] == '*' {
+				dp[i][j] = dp[i][j-2]
+				if s[i-1] == p[j-2] || p[j-2] == '.' {
+					dp[i][j] = dp[i-1][j] || dp[i][j]
+				}
+			}
+		}
+	}
+
+	return dp[m][n]
+}
+
+func isMatchV1(s string, p string) bool {
+
+	if len(s) == 0 {
+		index := 0
+		for index < len(p) {
+			if index+1 < len(p) && string(p[index+1]) == "*" {
+				index += 2
+			} else {
+				return false
+			}
+		}
+
+		return true
+	}
+	if len(p) <= 1 {
+		return len(s) == 1 && (s == p || p == ".")
+	}
+
+	if string(p[1]) == "*" {
+		if len(s) == 0 {
+			return isMatch(s, p[2:])
+		}
+		index := 0
+		canContinue := true
+		for index < len(s) && canContinue {
+			match := false
+			if string(p[0]) == "." || string(p[0]) == string(s[index]) {
+				match = isMatch(s[index+1:], p[2:]) || isMatch(s[index:], p[2:])
+			} else {
+				canContinue = false
+				match = isMatch(s[:], p[2:])
+			}
+			if match {
+				return true
+			}
+			index++
+		}
 		return false
 	}
 
-	strResult := ""
-
-	for x >= 10 {
-		module := x % 10
-		x = x / 10
-		strResult = digits[module] + strResult
-	}
-	strResult = digits[x] + strResult
-
-	i := 0
-	j := len(strResult) - 1
-
-	for i <= j {
-
-		if strResult[i] != strResult[j] {
-			return false
-		}
-		i++
-		j--
-	}
-
-	return true
+	return (string(p[0]) == string(s[0]) || string(p[0]) == ".") && isMatch(s[1:], p[1:])
 }
